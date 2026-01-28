5ff3882bc9878f785c96a5601ecfc46d4ca4578f
codevarthy <codevarthy@gmail.com>
2026-01-28 11:52:25 -0600

    Update gcs.tf

--- code/build/gcs.tf
+++ code/build/gcs.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "example" {
-  name          = "demo12-${random_id.rand_suffix.hex}"
+  name          = "demok-${random_id.rand_suffix.hex}"
   location      = "us-central1"
   force_destroy = true
 
